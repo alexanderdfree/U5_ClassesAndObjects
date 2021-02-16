76c5fb19857d02552d32b8e588e580e7b4b232e8
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-02-16 11:30:34 -0800

    Create Complex.java

---
+++ Complex.java
@@ -0,0 +1,9 @@
+public class Complex{
+   double re; //the real part
+   double im; //the imaginary part
+  
+   public Complex(double re, double im){
+      this.re = re;
+      this.im = im;
+  }
+}
